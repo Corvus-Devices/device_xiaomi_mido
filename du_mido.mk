7dc50f8a93bc7c308f03cc0903f3682e79657486
Rafiester <rafi.haqqi@gmail.com>
2020-02-28 22:40:54 +0700

    mido: Corvus-ify
    Welcum to our kenzur devices :D
    
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- du_mido.mk
+++ du_mido.mk
@@ -21,12 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CorvusOS stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Inherit build type
+DU_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := du_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
